a0276915ed4f1176bd63699a2dcc9afbcbf7e1f8
Mujtaba Anwar Shaikh <shaikhmujtaba072@gmail.com>
2025-07-23 16:06:37 +0530

    Add Terraform EC2 deployment files

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Ubuntu 22.04 LTS AMI"
+  default     = "ami-053b0d53c279acc90"
+}
+
+variable "repo_url" {
+  default = "https://github.com/techeazy-consulting/techeazy-devops.git"
+}
+
+variable "key_name" {
+  description = "Name of existing AWS key pair"
+  type        = string
+}
